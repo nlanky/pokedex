4556577c3ba892aa0d92f1e202a4c35f7bb230e3
Neil Lancaster <neiljlancaster@hotmail.co.uk>
2019-11-09 17:37:18 +0000

    Updates

---
+++ precache-manifest.26808a6c938a5b36e4c8319490714aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0a36aca12c3c24508a1124f06aa98d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d322428f015cb9ae5c57",
+    "url": "/pokedex/static/css/main.d0534e24.chunk.css"
+  },
+  {
+    "revision": "d4826f96e2d7228d46ed",
+    "url": "/pokedex/static/js/2.e3e70350.chunk.js"
+  },
+  {
+    "revision": "d322428f015cb9ae5c57",
+    "url": "/pokedex/static/js/main.f4cdefc3.chunk.js"
+  },
+  {
+    "revision": "27f16e0463aa7cf68513",
+    "url": "/pokedex/static/js/runtime-main.dc93633d.js"
+  }
+]);
